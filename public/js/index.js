5c985f8a26bdff7874add355b911778dfe7241a3
amandhembla@gmail.com <amandhembla@gmail.com>
2017-10-25 11:31:08 +0530

    add form for sending messages and display messages in the browser

--- public/js/index.js
+++ public/js/index.js
@@ -6,9 +6,22 @@ socket.on("connect",()=>{
 });
 
 socket.on("newMessage",function(data){
+  var li=jQuery("<li></li>");
+  li.text(`${data.from}:${data.text}`);
+  $("#message").append(li);
   console.log(data);
 })
 
 socket.on("disconnect",()=>{
   console.log("server disconnected from client");
 });
+
+
+jQuery("#message-form").on("submit",function(e){
+  e.preventDefault();
+
+  socket.emit("createMessage",{
+    from:"User",
+    text:jQuery("[name=message]").val()
+  });
+})
